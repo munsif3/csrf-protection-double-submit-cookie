615c4ac68362dca425373f51fe81eae1c4214c01
munsif <munsifmusthafa03@gmail.com>
2018-10-04 21:12:16 +0530

    Initial commit

---
+++ src/main/java/com/munsif/ssd/csrfdoublesubmit/CsrfdoublesubmitApplication.java
@@ -0,0 +1,12 @@
+package com.munsif.ssd.csrfdoublesubmit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrfdoublesubmitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsrfdoublesubmitApplication.class, args);
+	}
+}
